ef1f553e5444ebd6b065b82ddba3bffe665b8939
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-10-13 16:14:49 -0400

    created test for endpoints

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
-const { expect } = require('chai');
-const { table } = require('../db/knex');
-
+const {expect} = require('chai');
+const request  = require('supertest');
+const app  = require('../src/app');
+const fixtures = require('./fixtures');
 
 
 const knex = require('../db/knex');
@@ -8,15 +9,90 @@ const knex = require('../db/knex');
 describe('CRUD todos', () => {
     before(async () => {
         await knex.migrate.latest()
+        .then(() => {
+            return knex.seed.run();
+        })
     })
-
-    it('it works', () => {
-        console.log("Its working!!!");
-    });
-
 
     it('Selects all rows of data', async () => {
         let data = await knex.from('todos').select('*')
         expect(data.length).equal(data.length)
     })
+
+    it('List all Records', (done) => {
+       request(app)
+        .get('/api/v1/todos')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((resp) => {
+            expect(resp.body).to.be.a('array');
+            console.log(resp.body)
+            done();
+        })
+    });
+
+    it('Shows one task by id', (done) => {
+        request(app)
+            .get('/api/v1/todos/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((resp) => {
+                expect(resp.body).to.be.a('object');
+                done();
+            });
+    });
+
+    it('Create a record', (done) => {
+        request(app)
+            .post('/api/v1/todos')
+            .send(fixtures.todo)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((res) => {
+                expect(res.body).to.be.a('object')
+                console.log(res.body)
+                fixtures.todo.id = res.body.id;
+                expect(res.body).to.deep.equal(fixtures.todo);
+                done();
+            });
+    });
+
+    if('Can update a todo', (done) => {
+        fixtures.todo.description = "You can do it!";
+        request(app)
+            .put('/api/v1/todos/1')
+            .send(fixtures.todo)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((res) => {
+                expect(res.body).to.be.a('object')
+                console.log(res.body)
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.deep.equal(fixtures.todo);
+                done();
+            });
+    });
+    
+
+
+    it('Can Delete a todo by id', (done) => {
+        request(app)
+            .delete('/api/v1/todos/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((res) => {
+                expect(res.body).to.be.a('object')
+                console.log(res.body)
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.deep.equal({
+                    delete: true
+                });
+                done();
+            });
+    });
 });
---
+++ test/fixtures.js
@@ -0,0 +1,23 @@
+
+const todos = [
+        {
+        description: "this is note 1"
+      },{
+        description: "this is note 2"
+      }, {
+        description: "this is note 3"
+      }];
+
+
+const todo = {
+    "description": "Testing Testing 123"
+};
+
+
+
+
+
+module.exports = {
+    todos,
+    todo
+}
